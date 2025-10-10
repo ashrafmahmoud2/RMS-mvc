f67c813e289e991fc45aa7e91171d936bb28e1fa
ashrafmahmoud2 <mmoo19701@gmail.com>
2025-10-10 21:34:04 +0300

    feat: containerize project with Docker

---
+++ RMS.Web/Program.cs
@@ -0,0 +1,214 @@
+using System.Reflection;
+using UoN.ExpressiveAnnotations.NetCore.DependencyInjection;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.DataProtection;
+using Hangfire;
+using Hangfire.Dashboard;
+using HashidsNet;
+using Serilog;
+using Serilog.Context;
+using RMS.Web.Services;
+using RMS.Web.Core.Mapping;
+using RMS.Web.Settings;
+using RMS.Web.Seeds;
+using RMS.Web.Helpers;
+using ViewToHTML.Extensions;
+using WhatsAppCloudApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var isDevelopment = builder.Environment.IsDevelopment();
+
+string connectionString;
+
+if (isDevelopment)
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // ? Production (Docker) DB
+    var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "rmsdb";
+    var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "RMS";
+    var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? "Your_password123!";
+    connectionString = $"Server={dbHost},1433;Database={dbName};User Id=sa;Password={dbPassword};TrustServerCertificate=True;";
+}
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders()
+    .AddSignInManager<SignInManager<ApplicationUser>>();
+
+//builder.Services.Configure<IdentityOptions>(options =>
+//{
+//    options.Password.RequiredLength = 8;
+
+//    options.User.RequireUniqueEmail = true;
+//});
+
+builder.Services.AddDataProtection().SetApplicationName(nameof(RMS));
+builder.Services.AddSingleton<IHashids>(_ => new Hashids("f1nd1ngn3m0", minHashLength: 11));
+
+//builder.Services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
+
+builder.Services.AddTransient<IImageService, ImageService>();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.AddTransient<IEmailBodyBuilder, EmailBodyBuilder>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(1); // how long to keep session alive
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(MappingProfile)));
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection(nameof(CloudinarySettings)));
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection(nameof(MailSettings)));
+
+//builder.Services.AddWhatsAppApiClient(builder.Configuration);
+
+builder.Services.AddExpressiveAnnotations();
+
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
+builder.Services.AddHangfireServer();
+
+//builder.Services.Configure<AuthorizationOptions>(options =>
+//options.AddPolicy("AdminsOnly", policy =>
+//{
+//    policy.RequireAuthenticatedUser();
+//    policy.RequireRole(AppRoles.Admin);
+//}));
+
+builder.Services.AddViewToHTML();
+
+//builder.Services.AddMvc(options =>
+//    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute())
+//);
+
+//Add Serilog
+//Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+//builder.Host.UseSerilog();
+
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseExceptionHandler("/Home/Error");
+
+//app.UseStatusCodePages(async statusCodeContext =>
+//{
+//	// using static System.Net.Mime.MediaTypeNames;
+//	statusCodeContext.HttpContext.Response.ContentType = System.Net.Mime.MediaTypeNames.Text.Plain;
+
+//	await statusCodeContext.HttpContext.Response.WriteAsync(
+//		$"Status Code Page: {statusCodeContext.HttpContext.Response.StatusCode}");
+//});
+
+//app.UseStatusCodePagesWithRedirects("/Home/Error/{0}");
+app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    Secure = CookieSecurePolicy.Always
+});
+
+//app.Use(async (context, next) =>
+//{
+//    //context.Response.Headers.Add("X-Frame-Options", "Deny");
+
+//    await next();
+//});
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+
+//using var scope = scopeFactory.CreateScope();
+
+//var roleManger = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//var userManger = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+//await DefaultRoles.SeedAsync(roleManger);
+//await DefaultUsers.SeedAdminUserAsync(userManger);
+
+//hangfire
+app.UseHangfireDashboard("/hangfire", new DashboardOptions
+{
+    DashboardTitle = "RMS Dashboard",
+    IsReadOnlyFunc = (DashboardContext context) => true,
+    Authorization = new IDashboardAuthorizationFilter[]
+    {
+        new HangfireAuthorizationFilter("AdminsOnly")
+    }
+});
+
+//var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//var webHostEnvironment = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+//var whatsAppClient = scope.ServiceProvider.GetRequiredService<IWhatsAppClient>();
+//var emailBodyBuilder = scope.ServiceProvider.GetRequiredService<IEmailBodyBuilder>();
+//var emailSender = scope.ServiceProvider.GetRequiredService<IEmailSender>();
+
+//var hangfireTasks = new HangfireTasks(dbContext, webHostEnvironment, whatsAppClient,
+//    emailBodyBuilder, emailSender);
+
+//RecurringJob.AddOrUpdate(() => hangfireTasks.PrepareExpirationAlert(), "0 14 * * *");
+//RecurringJob.AddOrUpdate(() => hangfireTasks.RentalsExpirationAlert(), "0 14 * * *");
+
+//app.Use(async (context, next) =>
+//{
+//    LogContext.PushProperty("UserId", context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+//    LogContext.PushProperty("UserName", context.User.FindFirst(ClaimTypes.Name)?.Value);
+
+//    await next();
+//});
+
+//app.UseSerilogRequestLogging();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+
+app.Run();
---
+++ RMS.Web/ToDo.cs
@@ -0,0 +1,66 @@
+﻿/*
+#genral 
+- add data in ar end in en
+- don't forget to use enum gernallsettings
+- using UtcNow or now 
+- make gologal var like userid , selected branch id , 
+- Add placeholder images in product grid and modal
+
+
+#Branch
+- to handel BranchWorkingHourException , 
+- add duffel branch when open ,
+- add max allowd number of order in same time , and if allow make the 2 in kds , and in driver app as one trap
+
+#Options
+- add + and - as counter in topping-item button 
+- MaxAllowedOptions and MinAllowedOptions cannot exceed the total number of available options.
+- The maximum allowed quantity for an option cannot be greater than the MaxAllowedOptions of its group.
+
+#item
+- add fucntion and hangfire for make wakely update to ItemStatus in en and ar , allow to have to status in same card 
+- first iamges cant be loading="lazy"
+- add CardLabelsAr in ui
+-
+
+
+
+#Customer
+- Allow to just have one Defult Address in table CustomerAddress
+
+
+
+#OrderStatus
+- timestamp to creaton 
+- add created by chef or user ?
+
+
+#order details
+- handal cancel order affter time , and if not be visable if come from cutomerorders veiw
+
+
+#chekout
+- add max allowd number of order in same time
+- handel if add more then order like show 2 progress box , and add in kds 
+
+
+
+
+----------------------------------------------------------------------------------
+in resuten ui
+----------------------------------------------------------------------------------
+#General
+- Applay ui in ar en 
+
+
+#branch working hwour
+- rebuild this like talabat shifts
+- fix in client ui , and apply vildation when order
+- alart is  branch close like dirikit or jahhaz(apply this when add new table to make shifts like talbat)
+- search to allow isopen & is pasy or what 
+
+
+
+
+
+*/
